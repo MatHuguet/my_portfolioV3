4fe8b677aae491ab5c5dff93cebad9128d7ce2fc
MathieuH <huguet.mathieu@gmail.com>
2022-12-01 16:58:23 +0100

    set new ham position right + js IntersectionObs

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,33 @@
-//---HAMBURGER TOGGLE MENU
-const hamburger = document.querySelector(".hamburger");
-const navItems = document.querySelector(".nav-item");
+// //---HAMBURGER TOGGLE MENU
+// const hamburger = document.querySelector(".hamburger");
+// const navItems = document.querySelector(".nav-item");
 
-hamburger.addEventListener("click", () => {
-  console.log("click");
-});
+// hamburger.addEventListener("click", () => {
+//   console.log("click");
+// });
 
 //---STICKY-NAV
 
-const navBar = document.querySelector(".nav");
+const navBar = document.getElementById("navigationMenu");
+const picture = document.querySelector(".work-room");
+
+const options = {
+  root: null,
+  threshold: 0.44,
+  rootMargin: "25px",
+};
+// const topNav = window.addEventListener("scroll", (e) => {});
+// console.log(window.scrollY);
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry.isIntersecting);
+    // entry.target.classList.toggle("fixedNav");
+    if (!entry.isIntersecting) {
+      navBar.classList.toggle("navTopAnimation");
+    } else {
+      navBar.classList.remove("navTopAnimation");
+    }
+  });
+}, options);
+
+observer.observe(picture);
