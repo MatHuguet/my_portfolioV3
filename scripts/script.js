038d2538428ee098bf81c86193abe843e40ed564
MathieuH <huguet.mathieu@gmail.com>
2022-12-01 13:40:37 +0100

    Add new main image desk. Start script for ham + navbar

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+//---HAMBURGER TOGGLE MENU
+const hamburger = document.querySelector(".hamburger");
+const navItems = document.querySelector(".nav-item");
+
+hamburger.addEventListener("click", () => {
+  console.log("click");
+});
+
+//---STICKY-NAV
+
+const navBar = document.querySelector(".nav");
