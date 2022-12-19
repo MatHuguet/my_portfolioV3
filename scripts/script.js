94720e55455cda6b3eb503fe90219ba1c16c50af
MathieuH <huguet.mathieu@gmail.com>
2022-12-19 18:30:00 +0100

    changed ham menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,37 @@
 //---HAMBURGER TOGGLE MENU
 const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
 const navItems = document.querySelector(".nav-item");
-const topLine = document.querySelector(".top-line");
 const bottomLine = document.querySelector(".bottom-line");
 
 hamburger.addEventListener("click", () => {
-  topLine.classList.toggle("top-line-anim");
-  bottomLine.classList.toggle("bottom-line-anim");
+  hamburger.classList.toggle("ham-anim");
+  navList.classList.toggle("nav-list-toggle");
+  navItems.classList.toggle("nav-item-toggle");
 });
 
 //---STICKY-NAV
 
-const navBar = document.getElementById("navigationMenu");
-const picture = document.querySelector(".work-room");
+// const navBar = document.getElementById("navigationMenu");
+// const picture = document.querySelector(".work-room");
 
-const options = {
-  root: null,
-  threshold: 0,
-  rootMargin: "66px",
-};
-// const topNav = window.addEventListener("scroll", (e) => {});
-// console.log(window.scrollY);
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    // console.log(entry.isIntersecting);
-    // entry.target.classList.toggle("fixedNav");
-    if (!entry.isIntersecting) {
-      navBar.classList.toggle("navTopAnimation");
-    } else {
-      navBar.classList.remove("navTopAnimation");
-    }
-  });
-}, options);
+// const options = {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: "66px",
+// };
+// // const topNav = window.addEventListener("scroll", (e) => {});
+// // console.log(window.scrollY);
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach((entry) => {
+//     // console.log(entry.isIntersecting);
+//     // entry.target.classList.toggle("fixedNav");
+//     if (!entry.isIntersecting) {
+//       navBar.classList.toggle("navTopAnimation");
+//     } else {
+//       navBar.classList.remove("navTopAnimation");
+//     }
+//   });
+// }, options);
 
-observer.observe(picture);
+// observer.observe(picture);
